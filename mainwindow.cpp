d6bd35f1fd204088f63ceebce7ac0569c2aca673
ahmet cihan <acihana@gmail.com>
2020-05-09 14:08:17 +0300

    single output is ok

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,11 +8,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     double input[2];
-    double pre_output;
-    double output;
-    double w[9];
-    double A,B,C;
-    double sigmoid_A,sigmoid_B,sigmoid_C;
+    double Y_in,Y_out,delta_Y;
+    double w[9],delta_w[9];
+    double A_in,B_in,C_in;
+    double A_out,B_out,C_out;
+    double delta_A,delta_B,delta_C;
     double error;
     double desired_output;
 
@@ -20,51 +20,78 @@ MainWindow::MainWindow(QWidget *parent) :
     input[1] = 0;
     desired_output = 0;
 
-    qDebug() << " input[0] : " << input[0] << " input[1] : " << input[1];
+    qDebug() << " input[0] : " << input[0] << " input[1] : " << input[1] << "output : " << desired_output;
 
     for(u8 i = 0; i < 9; i++){
         w[i] = 0.1 * i + 0.1;
-        qDebug() << QString("w[%1] : ").arg(i) << w[i];
     }
 
-    A = input[0]*w[0] + input[1]*w[1];
-    B = input[0]*w[2] + input[1]*w[3];
-    C = input[0]*w[4] + input[1]*w[5];
+    A_in = input[0]*w[0] + input[1]*w[1];
+    B_in = input[0]*w[2] + input[1]*w[3];
+    C_in = input[0]*w[4] + input[1]*w[5];
 
-    sigmoid_A = sigmoid_func(A);
-    sigmoid_B = sigmoid_func(B);
-    sigmoid_C = sigmoid_func(C);
+    A_out = sigmoid_func(A_in);
+    B_out = sigmoid_func(B_in);
+    C_out = sigmoid_func(C_in);
 
-    pre_output = sigmoid_A*w[6] + sigmoid_B*w[7] + sigmoid_C*w[8];
+    Y_in = A_out*w[6] + B_out*w[7] + C_out*w[8];
+    Y_out = sigmoid_func(Y_in);
+    error = desired_output - Y_out;
 
-    output = sigmoid_func(pre_output);
-    error = desired_output - output;
+    delta_Y = derivative_of_sigmoid_func(Y_in) * error;
+    delta_A = delta_Y/w[6] * derivative_of_sigmoid_func(A_in);
+    delta_B = delta_Y/w[7] * derivative_of_sigmoid_func(B_in);
+    delta_C = delta_Y/w[8] * derivative_of_sigmoid_func(C_in);
 
-    qDebug() << " output : " << output << "error" << error;
+    qDebug() << "error :" << error;
 
-    double A_change_weight;
-    double B_change_weight;
-    double C_change_weight;
-    double w_change_weight[9];
+    if (input[0] == 0){
+        delta_w[0] = 0;
+        delta_w[2] = 0;
+        delta_w[4] = 0;
+    }
+    else{
+        delta_w[0] = delta_A/input[0];
+        delta_w[2] = delta_B/input[0];
+        delta_w[4] = delta_C/input[0];
+    }
 
-    A_change_weight = output / w[6];
-    A_change_weight = A_change_weight * derivative_of_sigmoid_func(A);
-    w_change_weight[6] = A_change_weight / sigmoid_A;
-    w[6] = w[6] + w_change_weight[6];
+    if (input[1] == 0){
+        delta_w[1] = 0;
+        delta_w[3] = 0;
+        delta_w[5] = 0;
+    }
+    else{
+        delta_w[1] = delta_A/input[1];
+        delta_w[3] = delta_B/input[1];
+        delta_w[5] = delta_C/input[1];
+    }
 
-    B_change_weight = output / w[7];
-    B_change_weight = B_change_weight * derivative_of_sigmoid_func(B);
-    w_change_weight[7] = B_change_weight / sigmoid_B;
-    w[7] = w[7] + w_change_weight[7];
+    if(A_out == 0){
+        delta_w[6] = 0;
+    }
+    else{
+        delta_w[6] = delta_Y / A_out;
+    }
 
-    C_change_weight = output / w[8];
-    C_change_weight = C_change_weight * derivative_of_sigmoid_func(C);
-    w_change_weight[8] = C_change_weight / sigmoid_C;
-    w[8] = w[8] + w_change_weight[8];
+    if(B_out == 0){
+        delta_w[7] = 0;
+    }
+    else{
+        delta_w[7] = delta_Y / B_out;
+    }
 
-    qDebug() << "w[6]" << w[6];
-    qDebug() << "w[7]" << w[7];
-    qDebug() << "w[8]" << w[8];
+    if(C_out == 0){
+        delta_w[8] = 0;
+    }
+    else{
+        delta_w[8] = delta_Y / B_out;
+    }
+
+    for(u8 i = 0; i < 9; i++){
+        w[i] = w[i] + delta_w[i];
+        qDebug() << QString("w[%1]").arg(i) << w[i];
+    }
 
 }
 double MainWindow::sigmoid_func(double val){
