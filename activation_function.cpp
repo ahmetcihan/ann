649069f99956a44fcd08796548a53c54c447b139
Ahmet Cihan AKINCA <acihana@gmail.com>
2020-05-14 17:02:54 +0300

    swish is implemented

--- activation_function.cpp
+++ activation_function.cpp
@@ -1,15 +1,16 @@
 #include "mainwindow.h"
 
 double MainWindow::sigmoid_func(double val){
-    //return (1 / (1 + exp(-val)));   //sigmoid   - good
-    //return tanh(val);             //tanh      - not worked
-    //return val;                   //identity  - not properly worked
-    //return atan(val);             //atan      - not bad but slower
-    //return (log(1+exp(val)));     //softplus  - good but slower
-    /***************RELU**********/ //ReLU      - very good
+    //return (1 / (1 + exp(-val)));     //sigmoid   - good
+    //return tanh(val);                 //tanh      - not worked
+    //return val;                       //identity  - not properly worked
+    //return atan(val);                 //atan      - not bad but slower
+    //return (log(1+exp(val)));         //softplus  - good but slower
+    /***************RELU**********/     //ReLU      - very good
     if(val < 0) return 0;
     if(val >= 0) return val;
     /*****************************/
+    //return (val / (1 + exp(-val)));     //swish     - not bad but not good
 }
 double MainWindow::derivative_of_sigmoid_func(double val){
     //return (sigmoid_func(val) * (1 - sigmoid_func(val)));       //sigmoid   - good
@@ -21,5 +22,6 @@ double MainWindow::derivative_of_sigmoid_func(double val){
     if(val < 0) return 0;
     if(val >= 0) return 1;
     /*****************************/
+    //return (1 + exp(-val) + val*exp(-val))/((1 + exp(-val))*(1 + exp(-val)));   //swish     - not bad but not good
 }
 
